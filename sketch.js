25bea6f25cfb6ac6df621a0e825bdaad38def4f6
Neeraj Mac <ntalukdar20@gmail.com>
2020-04-17 21:01:15 -0400

    Testing Divination

--- sketch.js
+++ sketch.js
@@ -1,63 +1,70 @@
-// Copyright (c) 2018 ml5
+// Copyright (c) 2019 ml5
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
 /* ===
-ML5 Example
-Interactive LSTM Text Generation Example using p5.js
+ml5 Example
+LSTM Generator example with p5.js
 This uses a pre-trained model on a corpus of Virginia Woolf
 For more models see: https://github.com/ml5js/ml5-data-and-training/tree/master/models/charRNN
 === */
 
 let charRNN;
 let textInput;
-let tempSlider;
 let lengthSlider;
+let tempSlider;
+let button;
 let runningInference = false;
 
 function setup() {
   noCanvas();
 
   // Create the LSTM Generator passing it the model directory
-  charRNN = ml5.charRNN('models/jkrowling_HP/', modelReady);
+  charRNN = ml5.charRNN('./models/woolf/', modelReady);
 
   // Grab the DOM elements
   textInput = select('#textInput');
   lengthSlider = select('#lenSlider');
   tempSlider = select('#tempSlider');
+  button = select('#generate');
 
-  // Run generate anytime something changes
-  textInput.input(generate);
-  lengthSlider.input(generate);
-  tempSlider.input(generate);
+  // DOM element events
+  button.mousePressed(generate);
+  lengthSlider.input(updateSliders);
+  tempSlider.input(updateSliders);
+}
+
+// Update the slider values
+function updateSliders() {
+  select('#length').html(lengthSlider.value());
+  select('#temperature').html(tempSlider.value());
 }
 
 function modelReady() {
   select('#status').html('Model Loaded');
 }
 
+// Generate new text
 function generate() {
   // prevent starting inference if we've already started another instance
   // TODO: is there better JS way of doing this?
  if(!runningInference) {
-   runningInference = true;
+    runningInference = true;
 
     // Update the status log
     select('#status').html('Generating...');
-
-    // Update the length and temperature span elements
-    select('#length').html(lengthSlider.value());
-    select('#temperature').html(tempSlider.value());
 
     // Grab the original text
     let original = textInput.value();
     // Make it to lower case
     let txt = original.toLowerCase();
 
-    // Check if there's something
+    // Check if there's something to send
     if (txt.length > 0) {
-      // Here is the data for the LSTM generator
+      // This is what the LSTM generator needs
+      // Seed text, temperature, length to outputs
+      // TODO: What are the defaults?
       let data = {
         seed: txt,
         temperature: tempSlider.value(),
@@ -67,17 +74,13 @@ function generate() {
       // Generate text with the charRNN
       charRNN.generate(data, gotData);
 
-      // Update the DOM elements with typed and generated text
+      // When it's done
       function gotData(err, result) {
+        // Update the status log
         select('#status').html('Ready!');
-        select('#original').html(original);
-        select('#prediction').html(result.sample);
+        select('#result').html(txt + result.sample);
         runningInference = false;
       }
-    } else {
-      // Clear everything
-      select('#original').html('');
-      select('#prediction').html('');
     }
   }
 }
